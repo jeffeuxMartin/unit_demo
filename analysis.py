902844dfd5d7c97ee6068524f580799df25cabc2
Jeff Chen <you@example.com>
2024-06-22 13:15:55 +0800

    讓我自己看得懂 & debug

---
+++ analysis.py
@@ -0,0 +1,230 @@
+import streamlit as st
+import numpy as np, pandas as pd
+import matplotlib.pyplot as plt, seaborn as sns
+
+from utils import (
+    troublesome_尻ㄙㄟ,
+    entropy欸尻ㄙㄟ,
+)
+
+@st.cache_resource
+def load_data(
+    model_type: str = "hubert",
+    cluster_num: int = 100,
+):
+    return np.load(
+        "./analysis_data/"
+       f"{model_type}/"
+       f"clu{cluster_num:03d}/"
+        "train-clean-100.npz"
+    )
+
+def parse_urlargs():
+    pass
+    urlmodeltype = st.query_params.get('modeltype', 'hubert')
+    inmymodeltype = st.text_input('modeltype', urlmodeltype)
+    if inmymodeltype != urlmodeltype:
+        st.query_params['modeltype']=inmymodeltype
+        urlmodeltype = inmymodeltype
+    mymodeltype = urlmodeltype
+
+    urlclu = st.query_params.get('clu', '100')
+    inmyclu = st.text_input('clu', urlclu)
+    if inmyclu != urlclu:
+        st.query_params['clu']=inmyclu
+        urlclu = inmyclu
+    clu = int(urlclu)
+    return mymodeltype, clu
+
+def plot_heatmap(
+    df: pd.DataFrame,
+    x_groups = None,
+    y_groups = None,
+    vmin = None,
+    vmax = None,
+    horizontal_highlighted = False,
+    vertical_highlighted = False,
+    annotated = False,
+  ):
+    plt.clf()
+    graph = plt.figure(figsize=(25, 10))
+    fig, ax = plt.subplots()
+    fig.set_size_inches(25, 10)
+
+    options = {}
+    if vmin is not None:
+        options["vmin"] = vmin
+    if vmax is not None:
+        options["vmax"] = vmax
+
+    
+
+    axis = sns.heatmap(
+        df,
+        ax=ax,
+        cmap="YlGnBu",
+        **options,
+    )
+    axis.set_xticklabels(
+        df.columns,
+        rotation=75,
+        fontsize=12,
+        # horizontalalignment='right',
+        minor = False,  # each 1
+    )
+    axis.set_xticks(
+        np.arange(df.shape[1]) + 0.5,
+        minor = False,
+    )
+
+    axis.set_yticklabels(
+        df.index,
+        rotation=0,
+        fontsize=12,
+        minor = False,  # each 1
+    )
+    axis.set_yticks(
+        np.arange(df.shape[0]) + 0.5,
+        minor = False,
+    )
+
+    if x_groups is not None:
+        for pos in np.cumsum(x_groups)[:-1]:
+            if vertical_highlighted:
+                axis.axvline(pos, color='r', linewidth=1, linestyle="-")
+            else:
+                axis.axvline(pos, color='blue', linewidth=0.5, linestyle="--")
+    if y_groups is not None:
+        for pos in np.cumsum(y_groups)[:-1]:
+            if horizontal_highlighted:
+                axis.axhline(pos, color='r', linewidth=1, linestyle="-")
+            else:
+                axis.axhline(pos, color='blue', linewidth=0.5, linestyle="--")
+    
+    if annotated:
+        for i in range(df.shape[0]):
+            for j in range(df.shape[1]):
+                axis.text(j + 0.5, i + 0.5, 
+                          f"{df.iloc[i, j] * 100:.2f} %",
+                          ha="center", va="center", 
+                          color="red", fontsize=18,
+                )
+    st.pyplot(fig)
+    plt.close(fig)
+    return fig
+
+def plot_barplot(
+    df: pd.DataFrame,
+    x: str,
+    y: str,
+):
+    ## TO check --- by Copilot
+    fig, ax = plt.subplots()
+    fig.set_size_inches(25, 10)
+
+    axis = sns.barplot(
+        x=x,
+        y=y,
+        data=df,
+        ax=ax,
+    )
+    axis.set_xticklabels(
+        axis.get_xticklabels(),
+        rotation=75,
+        fontsize=12,
+        horizontalalignment='right',
+    )
+    st.pyplot(fig)
+    plt.close(fig)
+    return fig
+
+def get_confusion_matrix(
+    probs: np.ndarray,
+    unit_groups: np.ndarray,
+    phn_groups: np.ndarray,
+):
+    unit_groups = np.array(unit_groups)
+    phn_groups = np.array(phn_groups)
+
+    def boundaries(x):
+        return zip(*([0] + x.tolist()[:-1], x.tolist()))
+
+    result = np.zeros((len(unit_groups), len(phn_groups)))
+    for ni, (bx_from, bx_to) in enumerate(
+                boundaries(phn_groups.cumsum())):
+        for nj, (by_from, by_to) in enumerate(
+                boundaries(unit_groups.cumsum())):
+            result[ni, nj] = probs[
+                bx_from : bx_to, 
+                by_from : by_to,
+            ].sum()
+    return result
+
+if __name__ == "__main__":
+    analysis_obj = parse_urlargs()
+    data = load_data(*analysis_obj)
+    st.markdown(f"## {analysis_obj[0]} with {analysis_obj[1]} clusters")
+
+    knowledge_data = pd.read_csv(
+        "./knowledge_table.tsv", sep="\t"
+    )
+
+    PHONETIC_GROUPS = (
+        knowledge_data['cls'].unique().tolist())
+    PHONETIC_GROUP_NUMS = [6, 2, 9, 3, 4, 12, 3, 2]  # FIXME
+
+
+    df__joint_prob = pd.DataFrame(
+        data.get("p_xy"),
+        index=data.get("ref2pid"),
+        columns=data.get("hyp2lid"),
+    )
+
+    ## df__joint_prob = (
+    ##     df__joint_prob.div(
+    ##         df__joint_prob.sum(axis=1), axis=0))
+
+
+    dfcool, resultgrouping = troublesome_尻ㄙㄟ(
+        df__joint_prob, knowledge_data)
+
+    plot_heatmap(
+        dfcool,
+        x_groups=resultgrouping,
+        y_groups=PHONETIC_GROUP_NUMS,
+        vmin=0.0, vmax=0.025,
+    )
+
+    confusion_matrix = get_confusion_matrix(
+        dfcool.values,
+        resultgrouping,
+        PHONETIC_GROUP_NUMS,
+    )
+
+    plot_heatmap(
+        pd.DataFrame(
+            confusion_matrix,
+            index=PHONETIC_GROUPS,
+            columns=PHONETIC_GROUPS,
+        ),
+        annotated=True,
+    )
+
+
+
+    df1, df = entropy欸尻ㄙㄟ(
+        df__joint_prob,
+        entropy_phn = data.get('h_each_x'),
+        entropy_unit = data.get('h_each_y'),
+        knowledge_data=knowledge_data,
+    )
+
+    plot_heatmap(
+        df1,
+        y_groups=PHONETIC_GROUP_NUMS,
+        vmin=0.0, 
+        # vmax=0.025,
+        horizontal_highlighted=True,
+    )
+
+    plot_heatmap(df)
---
+++ utils.py
@@ -0,0 +1,71 @@
+import pandas as pd
+
+def troublesome_尻ㄙㄟ(df, knowledge_data):
+    # st.write(df)
+    # st.write(df.T[knowledge_data['phn'].to_list()].T)
+    df1 = df.T[knowledge_data['phn'].to_list()].T
+    df2 = df1.T
+    # st.write(df2.T)
+    representative_phn_of_units = (df1.idxmax(axis=0))
+    max_prob_of_representative_phns = df1.max(axis=0)
+    representative_phn_of_units_idx = representative_phn_of_units.apply(lambda x: knowledge_data['phn'].to_list().index(x))
+    # df_sortedunits = df2.T.sort_values(by=representative_phn_of_units_idx, axis=1, ascending=True)
+    # st.write(representative_phn_of_units)
+    # st.write(max_prob_of_representative_phns)
+    # st.write(representative_phn_of_units_idx)
+    df_sort_by_aligned_phn_and_prob = pd.concat([representative_phn_of_units, max_prob_of_representative_phns, 
+            representative_phn_of_units_idx                                    ], axis=1)
+    df_sort_by_aligned_phn_and_prob.columns = ['phn', 'prob', 'phn_idx']
+    df_sort_by_aligned_phn_and_prob['sorter_value'] = (representative_phn_of_units_idx.max()+1-df_sort_by_aligned_phn_and_prob['phn_idx'] + max_prob_of_representative_phns / 100)
+    df_sort_by_aligned_phn_and_prob = df_sort_by_aligned_phn_and_prob.sort_values(by='sorter_value', ascending=False)
+    # st.write(df_sort_by_aligned_phn_and_prob)
+    dfcool = df1.loc[:, df_sort_by_aligned_phn_and_prob.index]
+    # plot_heatmap(dfcool)
+    # st.write(df1.loc[:, df_sort_by_aligned_phn_and_prob.index])
+
+    grouings_good = df2.idxmax(axis=1).value_counts()
+    # knowledge_data[['phn', 'cls']].groupby('phn').count()
+    phn_to_cls = knowledge_data[['phn', 'cls']].groupby('phn').first()
+    # grouings_good group by cls
+    grouings_good_cls = grouings_good.to_frame()
+    grouings_good_cls['cls1'] = grouings_good_cls.index.map(lambda x: phn_to_cls.loc[x, 'cls'])
+    grouings_good_cls = grouings_good_cls.reset_index()
+    grouings_good_cls['phn'] = grouings_good_cls['index']
+    grouings_good_cls = grouings_good_cls.drop(columns=['index'])
+    grouings_good_cls = grouings_good_cls[[
+        'cls1',
+        'phn',
+        'count',
+    
+    ]]
+    # grouings_good_cls = grouings_good_cls.join(phn_to_cls, on='index')
+    # sum 'count' by 'cls1'
+
+    grouings_good_cls = grouings_good_cls.groupby('cls1').aggregate('sum')
+    # if 0, add 0 to all cls
+    for it in knowledge_data['cls'].unique():
+        if it not in grouings_good_cls.index:
+            grouings_good_cls.loc[it] = 0
+    # st.write(grouings_good_cls)
+    mycls = (
+        knowledge_data['cls'].unique().tolist()
+    )
+    grouings_good_cls = (grouings_good_cls['count'])
+    resultgrouping = grouings_good_cls.loc[mycls].values.tolist()
+
+    return dfcool, resultgrouping
+
+def entropy欸尻ㄙㄟ(df, entropy_phn, entropy_unit, knowledge_data):
+
+    df['entropy_phn'] = entropy_phn
+    df = df.sort_values(by='entropy_phn', ascending=False)
+    df = df.drop(columns=['entropy_phn'])
+
+    df = df.T
+    df['entropy_unit'] = entropy_unit
+    df = df.sort_values(by='entropy_unit', ascending=False)
+    df = df.drop(columns=['entropy_unit'])
+    df = df.T
+
+    df1 = df.T[knowledge_data['phn'].to_list()].T
+    return df1, df
